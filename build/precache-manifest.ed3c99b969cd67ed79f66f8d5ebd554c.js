0847023df11f1c309df690f457313988513d451d
lhy <lhuanyutot@163.com>
2019-02-24 11:06:10 +0800

    build

---
+++ build/precache-manifest.ed3c99b969cd67ed79f66f8d5ebd554c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "193b153bf27024aed5d7",
+    "url": "/static/css/UserCenter.f2f0f49f.chunk.css"
+  },
+  {
+    "revision": "2078a7f7f1424b61a9a3",
+    "url": "/static/css/main.04d2e117.chunk.css"
+  },
+  {
+    "revision": "1c1933b71b96301d1631c4255c39d58d",
+    "url": "/static/media/music-icon.1c1933b7.svg"
+  },
+  {
+    "revision": "e76b1c9ba9b7e715428c",
+    "url": "/static/js/Rank.e76b1c9b.chunk.js"
+  },
+  {
+    "revision": "2a8a87a7d9d97d6ac2c8dd7388714f9b",
+    "url": "/static/media/music-icon.2a8a87a7.woff"
+  },
+  {
+    "revision": "1f542f5db2cbeb3f49b3",
+    "url": "/static/js/Search.1f542f5d.chunk.js"
+  },
+  {
+    "revision": "453725f541422e39397accbade389cea",
+    "url": "/static/media/music-icon.453725f5.ttf"
+  },
+  {
+    "revision": "ad312ed4838dcb904061",
+    "url": "/static/js/Singer.ad312ed4.chunk.js"
+  },
+  {
+    "revision": "2078a7f7f1424b61a9a3",
+    "url": "/static/js/main.2078a7f7.chunk.js"
+  },
+  {
+    "revision": "193b153bf27024aed5d7",
+    "url": "/static/js/UserCenter.193b153b.chunk.js"
+  },
+  {
+    "revision": "599485fb54bcfd3f927a",
+    "url": "/static/js/5.599485fb.chunk.js"
+  },
+  {
+    "revision": "850dbbd6356ab688db8d",
+    "url": "/static/js/runtime~main.850dbbd6.js"
+  },
+  {
+    "revision": "72f61f5619a4137f388581bc8ec1c34d",
+    "url": "/static/media/music-icon.72f61f56.eot"
+  },
+  {
+    "revision": "ad312ed4838dcb904061",
+    "url": "/static/css/Singer.00606d4a.chunk.css"
+  },
+  {
+    "revision": "1f542f5db2cbeb3f49b3",
+    "url": "/static/css/Search.ad06e017.chunk.css"
+  },
+  {
+    "revision": "e76b1c9ba9b7e715428c",
+    "url": "/static/css/Rank.fde8f06d.chunk.css"
+  },
+  {
+    "revision": "f94f14dfc1a1019fa2dcbcc945f063cf",
+    "url": "/index.html"
+  }
+];
